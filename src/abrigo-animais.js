ca999b6aa64caadb592828ff47fea6dd4783b481
yngridAMatos <matosingridy76@gmail.com>
2025-09-05 09:15:51 -0300

    Criacao da logica e correcao das constantes

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,13 +1,12 @@
+const ANIMAIS = {
+  Rex: {tipo: "cão", brinquedos: ["RATO", "BOLA"]},
+  Mimi: {tipo: "gato", brinquedos: ["BOLA", "LASER"]}// add o restante
+}
+
+const BRINQUEDOS_VALIDOS = new Set(
+    Object.values(ANIMAIS).flatMap(a => a.brinquedos)
+)
 class AbrigoAnimais {
-
-  const ANIMAIS = {
-    Rex: { tipo: "cão", brinquedos: ["RATO", "BOLA"]},
-    Mimi: {tipo: "gato", brinquedos: ["BOLA", "LASER"]}// add o restante
-  }
-
-  const BRINQUEDOS_VALIDOS = new Set(
-      Object.values(ANIMAIS).flatMap(a => a.brinquedos)
-  )
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
     try {
       const pessoa1 = brinquedosPessoa1.split(",").map(b => b.trim());
@@ -15,8 +14,28 @@ class AbrigoAnimais {
       const ordem = ordemAnimais.split(",").map(a => a.trim());
 
       this.validaAnimais(ordem);
+      this.validaBrinquedos([...pessoa1, ...pessoa2]);
 
+      let resultado = [];
+      let adotados = {pessoa1:0, pessoa2:0};
 
+      for (let animalNome of ordem) {
+        const animal = ANIMAIS[animalNome];
+        let pessoa1Pode = this.validaAdocao(animal, pessoa1, adotados.pessoa1);
+        let pessoa2Pode = this.validaAdocao(animal, pessoa2, adotados.pessoa2);
+
+        let destino = "Abrigo";
+        if(pessoa1Pode && !pessoa2Pode && adotados.pessoa1 < 3) {
+          destino = "Pessoa 1";
+          adotados.pessoa1++;
+        } else if(!pessoa1Pode && pessoa2Pode && adotados.pessoa2 < 3){
+          destino = "Pessoa 2";
+          adotados.pessoa2++;
+        }
+        resultado.push(`${animalNome} - ${destino}`);
+      }
+      resultado.sort((a, b) => a.localeCompare(b));
+      return {lista: resultado};
     } catch (e) {
       return {erro: e.message};
     }
@@ -25,8 +44,8 @@ class AbrigoAnimais {
   validaAnimais(ordem) {
     const set = new Set();
     for (let nome of ordem) {
-      if(!ANIMAIS[nome]) throw new Error("Animal Inválido");
-      if(set.has(nome)) throw new Error("Animal Inválido");
+      if(!ANIMAIS[nome]) throw new Error("Animal inválido");
+      if(set.has(nome)) throw new Error("Animal inválido");
       set.add(nome);
     }
   }
@@ -34,10 +53,23 @@ class AbrigoAnimais {
   validaBrinquedos(listaBrinquedos) {
     const set = new Set();
     for (let b of listaBrinquedos) {
-      if(!BRINQUEDOS_VALIDOS.has(b)) throw new Error("Brinquedo Inválido");
-      if(set.has(b)) throw new Error("Brinquedo Inválido");
+      if(!BRINQUEDOS_VALIDOS.has(b)) throw new Error("Brinquedo inválido");
+      if(set.has(b)) throw new Error("Brinquedo inválido");
       set.add(nome);
     }
+  }
+
+  validaAdocao(animal, brinquedosPessoa, qtdAtualAnimais) {
+    if(qtdAtualAnimais >= 3) return false;
+    if(animal == ANIMAIS["Loco"]) return true; //não importa a ordem dos brinquedos
+    let index = 0;
+    for (let b of brinquedosPessoa) {
+      if(b === animal.brinquedos[index]) {
+        index++;
+        if (index === animal.brinquedos.length) return true;
+      }
+    }
+    return false;
   }
 }
 
