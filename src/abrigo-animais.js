263aff47da39c441369011e910a110edc8b3b540
yngridAMatos <matosingridy76@gmail.com>
2025-09-05 10:25:26 -0300

    Add registro de todos animais

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,11 +1,17 @@
 const ANIMAIS = {
   Rex: {tipo: "cão", brinquedos: ["RATO", "BOLA"]},
-  Mimi: {tipo: "gato", brinquedos: ["BOLA", "LASER"]}// add o restante
-}
+  Mimi: {tipo: "gato", brinquedos: ["BOLA", "LASER"]},
+  Fofo: {tipo: "gato", brinquedos: ["BOLA", "RATO", "LASER"]},
+  Zero: {tipo: "gato", brinquedos: ["RATO", "BOLA"]},
+  Bola: { tipo: "cão", brinquedos: ["CAIXA", "NOVELO"]},
+  Bebe: {tipo: "cão", brinquedos: ["LASER", "RATO", "BOLA"]},
+  Loco: {tipo: "jabuti", brinquedos: ["SKATE", "RATO"]}
+};
 
 const BRINQUEDOS_VALIDOS = new Set(
     Object.values(ANIMAIS).flatMap(a => a.brinquedos)
-)
+);
+
 class AbrigoAnimais {
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
     try {
